3c1dfb4da95e4c38f3eb6d37aec52f459f05b4d7
Abhishek N V <abhicnv007@gmail.com>
2020-05-23 12:38:46 +0530

    added benchmarks for add and get

--- goku_test.go
+++ goku_test.go
@@ -1,9 +1,12 @@
 package main
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+)
 
 func TestAdd(t *testing.T) {
-	g := New()
+	g := New("")
 	g.Add("Q", "there")
 	g.Add("W", "here")
 	g.Add("E", "everywhere")
@@ -18,7 +21,7 @@ func TestAdd(t *testing.T) {
 }
 
 func TestGet(t *testing.T) {
-	g := New()
+	g := New("")
 	keys := []string{"A", "B", "C", "D"}
 	values := []string{"1", "2", "3", "4"}
 
@@ -31,5 +34,45 @@ func TestGet(t *testing.T) {
 		if !ok || v != values[i] {
 			t.Errorf("Goku Get = %s, want %s, got %s", k, values[i], v)
 		}
+	}
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+func BenchmarkAdd(b *testing.B) {
+	g := New("")
+	// create some random strings to be used as keys and values
+	randStrs := make([][]string, 0, b.N)
+	for i := 0; i < b.N; i++ {
+		randStrs = append(randStrs, []string{RandStringBytes(10), RandStringBytes(100)})
+	}
+	// reset timer and benchmark add
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		g.Add(randStrs[i][0], randStrs[i][1])
+	}
+}
+
+func BenchmarkGet(b *testing.B) {
+	g := New("")
+	// create some random strings to be used as keys and values
+	randStrs := make([][]string, 0, b.N)
+	for i := 0; i < b.N; i++ {
+		k, v := RandStringBytes(10), RandStringBytes(100)
+		randStrs = append(randStrs, []string{k, v})
+		g.Add(k, v)
+	}
+	// reset timer and benchmark add
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		g.Get(randStrs[i][0])
 	}
 }
